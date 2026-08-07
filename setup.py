e65216ac771e0ef374037b55f55f6a288daec99e
A L T <165473436+DogeDev010@users.noreply.github.com>
2026-08-07 02:03:15 -0500

    Update setup.py
    ?

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='toutatis',
-    version="1.31",
+    name='toutatis-updated',
+    version="1.32",
     packages=find_packages(),
-    author="megadose",
+    author="DogeDev010",
     install_requires=["argparse","requests","phonenumbers","pycountry"],
     description="It is a tool written to retrieve private information such as Phone Number, Mail Address, ID on Instagram accounts via API.",
     long_description="It is a tool written to retrieve private information such as Phone Number, Mail Address, ID on Instagram accounts via API.",
     include_package_data=True,
-    url='http://github.com/megadose/toutatis',
+    url='https://github.com/DogeDev010/toutatis-updated',
     entry_points = {'console_scripts': ['toutatis = toutatis.core:main']},
     classifiers=[
         "Programming Language :: Python",
